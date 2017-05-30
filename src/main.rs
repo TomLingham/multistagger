0a62c7bff8c5da08fc402e1bc06ecf0173f99dfe
Tom Lingham <tjlingham@gmail.com>
2017-05-30 23:17:43 +1000

    Some beginnings for COPY

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,28 @@ fn init_stage<'a>(register: &mut Vec<&'a str>, command: &'a str) -> &'a str {
         None => (),
     };
 
-    return command
+    command
+}
+
+
+fn prep_copy<'a, 'b>(stages_register: &mut BTreeMap<&str, Vec<String>>, command: &'a str) -> &'a str {
+    let capture = Regex::new(r#"^COPY +--from=([^\s]+) ("[^"]+"|[^\s"]+) +("[^"]+"|[^\s"]+)$"#).unwrap()
+        .captures(command);
+
+    match capture {
+        Some(x) => {
+            let stage_name = x.get(1).unwrap().as_str();
+            let from_file = x.get(2).unwrap().as_str();
+            let to_file = x.get(3).unwrap().as_str();
+
+            let step = format!(r#"COPY "{}" ".~multistagger/{}""#, from_file, to_file);
+
+            stages_register.get_mut(stage_name).unwrap().push(step);
+        },
+        None => (),
+    };
+
+    command
 }
 
 
@@ -37,25 +58,25 @@ fn main() {
     let command_reg = Regex::new(r"^([A-Z]+)").unwrap();
 
     let mut stages: Vec<&str> = vec![];
-    let mut stages_register: BTreeMap<&str, Vec<&str>> = BTreeMap::new();
+    let mut stages_register: BTreeMap<&str, Vec<String>> = BTreeMap::new();
 
     // Just incase there is white space at the start and end of the line
     let from_and_copy: Vec<&str> = split.into_iter()
         .map(|x| x.trim())
         .filter(|x| !x.starts_with("#")) // remove comments
         .map(|x| {
-            let stage = match command_reg.captures(x).unwrap().get(1).unwrap().as_str() {
+            let step = match command_reg.captures(x).unwrap().get(1).unwrap().as_str() {
                 "FROM" => {
                     init_stage(&mut stages, x);
                     stages_register.insert(stages.last().unwrap(), vec![]);
                     x
                 },
                 "COPY" => {
-                    // TODO: Implement this stuff...
+                    prep_copy(&mut stages_register, x)
                 }
                 _ => x
             };
-            stages_register.get_mut(stages.last().unwrap()).unwrap().push(stage);
+            stages_register.get_mut(stages.last().unwrap()).unwrap().push(step.to_owned());
             x
         })
         .collect();
